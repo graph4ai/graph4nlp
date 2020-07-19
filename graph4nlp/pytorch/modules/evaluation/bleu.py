70708e52d45c70a0816ff25719151630fa1568d5
AlanSwift <shenkai200861@gmail.com>
2020-07-19 09:36:56 +0000

    move test codes to test.evaluation

--- graph4nlp/pytorch/modules/evaluation/bleu.py
+++ graph4nlp/pytorch/modules/evaluation/bleu.py
@@ -71,29 +71,3 @@ class BLEU(EvaluationMetricBase):
         if not n_grams_ok:
             raise TypeError("argument n_grams must be in list of int.")
         return max(n_grams)
-
-
-if __name__ == "__main__":
-    import json
-
-    scorer = BLEU(n_grams=[1, 2, 3, 4])
-    pred_file_path = "/home/shiina/shiina/question/iq/pred.json"
-    gt_file_path = "/home/shiina/shiina/question/iq/gt.json"
-    with open(gt_file_path, "r") as f:
-        gt = json.load(f)
-        print(gt[0])
-        gts = []
-        for i in gt:
-            for j in i:
-                gts.append(str(j))
-    with open(pred_file_path, "r") as f:
-        pred = json.load(f)
-        print(pred[1])
-        preds = []
-        for i in pred:
-            for j in i:
-                preds.append(str(j))
-    print(len(gts), len(preds))
-    score, scores = scorer.calculate_scores(gts, preds)
-    print(score)
-    print(len(scores))
