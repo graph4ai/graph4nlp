b512300d028d30b17420794bbd4e352a22fd6bed
xguo7 <43113814+xguo7@users.noreply.github.com>
2020-07-31 20:32:41 -0400

    Update StackedElementProd.py

--- graph4nlp/pytorch/modules/prediction/classification/link_prediction/StackedElementProd.py
+++ graph4nlp/pytorch/modules/prediction/classification/link_prediction/StackedElementProd.py
@@ -62,7 +62,7 @@ class StackedElementProd(NodeClassifierBase):
         src_idx=torch.tensor(node_idx_list).view(-1,1).repeat(1,num_node).view(-1)
         dst_idx=torch.tensor(node_idx_list).view(1,-1).repeat(num_node,1).view(-1)
         input_graph.add_edges(src_idx,dst_idx)
-        input_graph.edge_features['logits']=self.classifier(node_emb_list)       
+        input_graph.edge_features['logits']=self.classifier(node_emb_list).clone().detach()       
         
         return input_graph
 
