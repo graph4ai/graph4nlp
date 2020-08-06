b453df32dbaf5dcaf0b103dcadc464bb576da6c1
Saizhuo <877562038@qq.com>
2020-08-06 10:52:58 +0800

    Partial implementation of Jobs Dataset.

--- graph4nlp/pytorch/data/dataset.py
+++ graph4nlp/pytorch/data/dataset.py
@@ -71,6 +71,7 @@ class Dataset(torch.utils.data.Dataset):
             return self.len()
 
     # TODO: Design the `self.indices` method.
+
     # TODO: Vectorization of data (step 3 of preprocessing). Runtime or static?
 
     def __getitem__(self, index):
@@ -111,6 +112,73 @@ class TopologyDataset(Dataset):
 
     def __init__(self, root, topology_build_fn, vocab_build_fn):
         super(TopologyDataset, self).__init__(root=root)
+
+        self.vocab_build_fn = vocab_build_fn
+        self.topology_build_fn = topology_build_fn
+
+        if 'build_topology' in self.__class__.__dict__.keys():
+            self._build_topology()
+
+        if 'build_vocab' in self.__class__.__dict__.keys():
+            self._build_vocab()
+
+    @property
+    def topology_dir(self):
+        return os.path.join(self.root, 'topology')
+
+    @property
+    def vocab_dir(self):
+        return os.path.join(self.root, 'vocab')
+
+    @property
+    def topology_file_paths(self):
+        return [os.path.join(self.topology_dir, file_name) for file_name in self.topology_file_names]
+
+    @property
+    def vocab_file_paths(self):
+        return [os.path.join(self.vocab_dir, file_name) for file_name in self.vocab_file_names]
+
+    def _build_topology(self):
+        if all([os.path.exists(file_path) for file_path in self.topology_file_paths]):
+            return
+
+        os.makedirs(self.topology_dir, exist_ok=True)
+        self.build_topology()
+
+    def _build_vocab(self):
+        if all([os.path.exists(file_path) for file_path in self.vocab_file_paths]):
+            return
+
+        os.makedirs(self.vocab_dir, exist_ok=True)
+        self._build_vocab()
+
+class TopologyBuilder(object):
+    """
+    The base class for dataset with topology building techniques.
+    """
+
+    @property
+    def topology_file_names(self):
+        """The file names of the topological data."""
+        raise NotImplementedError
+
+    @property
+    def vocab_file_names(self):
+        """The file names of the vocabulary."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def build_topology(self):
+        """Build topology based on raw data."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def build_vocab(self):
+        """Build vocabulary based on the topology."""
+        raise NotImplementedError
+
+    def __init__(self, topology_build_fn, vocab_build_fn):
+        super(TopologyBuilder, self).__init__()
 
         self.vocab_build_fn = vocab_build_fn
         self.topology_build_fn = topology_build_fn
--- graph4nlp/pytorch/datasets/jobs.py
+++ graph4nlp/pytorch/datasets/jobs.py
@@ -2,4 +2,15 @@ from ..data.dataset import Dataset
 
 dataset_root = '../test/dataset/jobs'
 
-class JobsDataset()
+
+class JobsDataset(Dataset):
+    def raw_file_names(self) -> list:
+        return ['test.txt', 'train.txt', 'vocab.f.txt', 'vocab.q.txt']
+
+    def download(self):
+        raise NotImplementedError(
+            'This dataset is now under test and cannot be downloaded. Please prepare the raw data yourself.')
+
+    def __init__(self, root_dir='../test/dataset/jobs'):
+        super(JobsDataset, self).__init__(root_dir)
+
