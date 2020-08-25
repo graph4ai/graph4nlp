549506e5516e013a47501b6f3c4bfd08d82b0760
gaohanning <919120351@qq.com>
2020-08-25 09:13:45 +0800

    add exception process

--- graph4nlp/pytorch/datasets/cnn.py
+++ graph4nlp/pytorch/datasets/cnn.py
@@ -4,8 +4,9 @@ import torch
 import pickle
 import json
 from graph4nlp.pytorch.data.dataset import Text2TextDataset, Text2TextDataItem
+from graph4nlp.pytorch.modules.utils.vocab_utils import VocabModel
 from ..modules.graph_construction.ie_graph_construction import IEBasedGraphConstruction
-
+import stanfordcorenlp
 dataset_root = '../../../examples/pytorch/summarization/cnn'
 
 
@@ -29,6 +30,68 @@ class CNNDataset(Text2TextDataset):
         super(CNNDataset, self).__init__(root_dir=root_dir, topology_builder=topology_builder,
                                           topology_subdir=topology_subdir, graph_type=graph_type,
                                           edge_strategy=edge_strategy, merge_strategy=merge_strategy, **kwargs)
+
+    def build_topology(self, data_items):
+        """
+        Build graph topology for each item in the dataset. The generated graph is bound to the `graph` attribute of the
+        DataItem.
+        """
+        if self.graph_type == 'static':
+            print('Connecting to stanfordcorenlp server...')
+            processor = stanfordcorenlp.StanfordCoreNLP('http://localhost', port=9000, timeout=1000)
+            print('CoreNLP server connected.')
+            pop_idxs = []
+            for idx, item in enumerate(data_items):
+                try:
+                    print(idx)
+                    graph = self.topology_builder.topology(raw_text_data=item.input_text, nlp_processor=processor,
+                                                           merge_strategy=self.merge_strategy,
+                                                           edge_strategy=self.edge_strategy,
+                                                           verbase=False)
+                    item.graph = graph
+                    if idx%100==1:
+                        torch.save(data_items, 'cnn_topo_tmp.pt')
+                except:
+                    pop_idxs.append(idx)
+                    item.graph = None
+                    print('item does not have graph: '+str(idx))
+
+            data_items = [x for idx, x in enumerate(data_items) if idx not in pop_idxs]
+            return data_items
+        elif self.graph_type == 'dynamic':
+            # TODO: Implement this
+            pass
+        else:
+            raise NotImplementedError('Currently only static and dynamic are supported!')
+
+    def _process(self):
+        if all([os.path.exists(processed_path) for processed_path in self.processed_file_paths.values()]):
+            if 'val_split_ratio' in self.__dict__:
+                UserWarning(
+                    "Loading existing processed files on disk. Your `val_split_ratio` might not work since the data have"
+                    "already been split.")
+            return
+
+        os.makedirs(self.processed_dir, exist_ok=True)
+
+        self.read_raw_data()
+
+        self.train = self.build_topology(self.train)
+        self.test = self.build_topology(self.test)
+        if 'val' in self.__dict__:
+            self.val = self.build_topology(self.val)
+
+        self.build_vocab()
+
+        self.vectorization(self.train)
+        self.vectorization(self.test)
+        if 'val' in self.__dict__:
+            self.vectorization(self.val)
+
+        data_to_save = {'train': self.train, 'test': self.test}
+        if 'val' in self.__dict__:
+            data_to_save['val'] = self.val
+        torch.save(data_to_save, self.processed_file_paths['data'])
 
     def parse_file(self, file_path) -> list:
         """
@@ -68,6 +131,6 @@ class CNNDataset(Text2TextDataset):
 
 if __name__ == '__main__':
     jd = CNNDataset(root_dir=dataset_root, topology_builder=IEBasedGraphConstruction,
-                topology_subdir='IEGraph')
+                topology_subdir='IEGraph2')
 
     a = 0
