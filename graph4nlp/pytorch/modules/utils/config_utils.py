a98edecb70a4eea1253bd2c720c42a3dca082132
AlanSwift <shenkai200861@gmail.com>
2020-11-08 23:38:09 +0800

    fix bugs

--- graph4nlp/pytorch/modules/utils/config_utils.py
+++ graph4nlp/pytorch/modules/utils/config_utils.py
@@ -3,7 +3,7 @@ import yaml
 
 def update_values(dict_from, dict_to):
     for key, value in dict_from.items():
-        if isinstance(value, dict):
+        if isinstance(value, dict) and key in dict_to.keys():
             update_values(dict_from[key], dict_to[key])
         elif value is not None:
             dict_to[key] = dict_from[key]
