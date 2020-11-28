10c7d6b4cddb1bc64d9a85aa8f283ac4ede0e843
AlanSwift <shenkai200861@gmail.com>
2020-11-28 16:20:56 +0800

    fix

--- build_and_upload_to_testpypi.sh
+++
@@ -1,4 +0,0 @@
-rm -rf build dist
-python setup.py bdist_wheel --universal
-python setup.py sdist
-twine upload --repository testpypi dist/*
--- graph4nlp/pytorch/test/seq_decoder/graph2seq/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd src
-python sample_test.py
