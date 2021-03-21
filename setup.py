2fbac88ab294dde47ee083db87094d66413a8bda
Saizhuo <swangeh@cse.ust.hk>
2021-03-21 16:42:54 +0800

    Updated README.md for building and distributing guide.

--- setup.py
+++ setup.py
@@ -19,15 +19,19 @@ if __name__ == '__main__':
 
     # import argparse
     # parser = argparse.ArgumentParser()
-    # parser.add_argument('--cuda_version', type=str, default='none')
-    # args = parser.parse_args()
+    # parser.add_argument('--cuda_version', type=str, default='cpu')
+    # args, unknown = parser.parse_known_args()
     # args = vars(args)
+    # cuda_version = args.get('cuda_version')
 
     cuda_version = input("CUDA Version('none' if you are using CPU): ")
+
     if cuda_version == 'none':
         cuda_version = ''
-    else:
+    elif cuda_version.find('.') != -1:
         cuda_version = '-cu' + ''.join(cuda_version.split('.'))
+    else:
+        cuda_version = ''
 
     install_requirement = ['torch >= 1.6.0', 'pythonds', 'nltk >= 3.5', 'stanfordcorenlp', 'scipy >= 1.5.2',
                            'scikit-learn >= 0.23.2', 'networkx >= 2.5', 'dgl{} >= 0.4'.format(cuda_version),
