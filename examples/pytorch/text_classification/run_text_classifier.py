2a2969fd96ab2f26cd07e5cb4ff6d5ab76fb187c
AlanSwift <shenkai200861@gmail.com>
2021-04-14 14:42:40 +0000

    fix bugs

--- examples/pytorch/text_classification/run_text_classifier.py
+++ examples/pytorch/text_classification/run_text_classifier.py
@@ -1,4 +1,5 @@
 import os
+# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 import time
 import datetime
 import argparse
@@ -296,6 +297,7 @@ class ModelHandler:
             t0 = time.time()
             for i, data in enumerate(self.train_dataloader):
                 tgt = to_cuda(data['tgt_tensor'], self.config['device'])
+                data['graph_data'] = data['graph_data'].to(self.config['device'])
                 logits, loss = self.model(data['graph_data'], tgt, require_loss=True)
                 self.optimizer.zero_grad()
                 loss.backward()
@@ -325,6 +327,7 @@ class ModelHandler:
             gt_collect = []
             for i, data in enumerate(dataloader):
                 tgt = to_cuda(data['tgt_tensor'], self.config['device'])
+                data['graph_data'] = data['graph_data'].to(self.config["device"])
                 logits = self.model(data['graph_data'], require_loss=False)
                 pred_collect.append(logits)
                 gt_collect.append(tgt)
