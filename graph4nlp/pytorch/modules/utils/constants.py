affdc98b54e5a53913b8c15516c056dffb0cc480
Yu Chen <hugochan2013@gmail.com>
2021-04-21 01:25:54 -0400

    fix dynamic graph gradient nan

--- graph4nlp/pytorch/modules/utils/constants.py
+++ graph4nlp/pytorch/modules/utils/constants.py
@@ -7,7 +7,7 @@ Module to handle universal/general constants used across files.
 ################################################################################
 
 # GENERAL CONSTANTS:
-VERY_SMALL_NUMBER = 1e-31
+VERY_SMALL_NUMBER = 1e-10
 INF = 1e20
 
 
