01b94cc36e1dea40197931580f40ba7f01861bd7
schenglee <scheng_lee@qq.com>
2021-05-12 23:32:45 +0800

    update

--- examples/pytorch/semantic_parsing/graph2tree/jobs/src/runner.py
+++ examples/pytorch/semantic_parsing/graph2tree/jobs/src/runner.py
@@ -111,6 +111,7 @@ class Jobs:
         self.tgt_vocab = dataset.tgt_vocab_model
         if self.use_share_vocab:
             self.share_vocab = dataset.share_vocab_model
+            print(self.share_vocab)
 
     def _build_model(self):
         '''For encoder-decoder'''
