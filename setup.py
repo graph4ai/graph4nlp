6ac1a620d916597c5358f73878084017fdc22ac0
SaizhuoWang <swangeh@conenct.ust.hk>
2021-06-04 16:30:33 +0800

    fix

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
 
     setup(
         name='graph4nlp{}'.format(cuda_version),
-        version='0.2a7',
+        version='0.2a8',
         description='A DGL and PyTorch based graph deep learning library for natural language processing',
         author='Graph4NLP Team',
         license='Apache 2.0',
