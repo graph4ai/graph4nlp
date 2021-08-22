570703b7fc5524bf234e9a29a564bbf17afbe168
gaohanning <919120351@qq.com>
2021-08-22 22:48:56 +0800

    fix code

--- graph4nlp/pytorch/modules/graph_embedding/base.py
+++ graph4nlp/pytorch/modules/graph_embedding/base.py
@@ -3,22 +3,22 @@ import torch.nn as nn
 
 
 class GNNLayerBase(nn.Module):
-    """Base class for GNN layer.
-    """
+    """Base class for GNN layer."""
+
     def __init__(self):
         super(GNNLayerBase, self).__init__()
 
     @abc.abstractmethod
     def forward(self, graph, node_feat):
-        raise NotImplementedError('GNNLayerBase: Not Implemented.')
+        raise NotImplementedError("GNNLayerBase: Not Implemented.")
 
 
 class GNNBase(nn.Module):
-    """Base class for GNN module.
-    """
+    """Base class for GNN module."""
+
     def __init__(self):
         super(GNNBase, self).__init__()
 
     @abc.abstractmethod
     def forward(self, graph, node_feat):
-        raise NotImplementedError('GNNBase: Not Implemented.')
+        raise NotImplementedError("GNNBase: Not Implemented.")
