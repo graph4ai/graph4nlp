c2639727ebc86a01958f527b77bf7817979dab8f
gaohanning <919120351@qq.com>
2022-02-24 23:03:21 +0800

    update cnn config

--- examples/pytorch/summarization/cnn/inference_advance.py
+++ examples/pytorch/summarization/cnn/inference_advance.py
@@ -1,4 +1,11 @@
+"""
+    The advanced inference code.
+    In this file, we will run the inference by writing the whole inference pipeline.
+    Compared with the inference.py, it is more efficient. It will save the graphs \
+        during inference, which support multi-processing when converting the raw inputs to graphs.
+"""
 import argparse
+import copy
 import os
 import time
 import numpy as np
@@ -9,84 +16,66 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import DataLoader
 
 from graph4nlp.pytorch.datasets.cnn import CNNDataset
-from graph4nlp.pytorch.modules.config import get_basic_args
 from graph4nlp.pytorch.modules.evaluation.rouge import ROUGE
 from graph4nlp.pytorch.modules.utils import constants as Constants
-from graph4nlp.pytorch.modules.utils.config_utils import get_yaml_config, update_values
+from graph4nlp.pytorch.modules.utils.config_utils import load_json_config
 from graph4nlp.pytorch.modules.utils.copy_utils import prepare_ext_vocab
-from graph4nlp.pytorch.modules.utils.generic_utils import EarlyStopping, to_cuda
+from graph4nlp.pytorch.modules.utils.generic_utils import all_to_cuda
 from graph4nlp.pytorch.modules.utils.logger import Logger
 from graph4nlp.pytorch.modules.utils.summarization_utils import wordid2str
 
-from main import SumModel
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
-
-
-def all_to_cuda(data, device=None):
-    if isinstance(data, torch.Tensor):
-        data = to_cuda(data, device)
-    elif isinstance(data, (list, dict)):
-        keys = range(len(data)) if isinstance(data, list) else data.keys()
-        for k in keys:
-            if isinstance(data[k], torch.Tensor):
-                data[k] = to_cuda(data[k], device)
-
-    return data
+from .main import SumModel
 
 
 class ModelHandler:
     def __init__(self, config):
         super(ModelHandler, self).__init__()
         self.config = config
-        self.use_copy = self.config["decoder_args"]["rnn_decoder_share"]["use_copy"]
-        self.use_coverage = self.config["decoder_args"]["rnn_decoder_share"]["use_coverage"]
+        self.use_copy = self.config["model_args"]["decoder_args"]["rnn_decoder_share"]["use_copy"]
+        self.use_coverage = self.config["model_args"]["decoder_args"]["rnn_decoder_share"][
+            "use_coverage"
+        ]
+        log_config = copy.deepcopy(config)
+        del log_config["env_args"]["device"]
         self.logger = Logger(
-            config["out_dir"],
-            config={k: v for k, v in config.items() if k != "device"},
+            config["checkpoint_args"]["out_dir"],
+            config=log_config,
             overwrite=True,
         )
-        self.logger.write(config["out_dir"])
-
-        self.stopper = EarlyStopping(
-            os.path.join(
-                "/raid/user8/graph4nlp", self.config["out_dir"], Constants._SAVED_WEIGHTS_FILE
-            ),
-            patience=self.config["patience"],
-        )
+        self.logger.write(config["checkpoint_args"]["out_dir"])
         self._build_model()
         self._build_dataloader()
-
-        self._build_optimizer()
         self._build_evaluation()
 
     def _build_dataloader(self):
-
         para_dic = {
-            "root_dir": self.config["graph_construction_args"]["graph_construction_share"][
+            "root_dir": self.config["model_args"]["graph_construction_args"]["graph_construction_share"][
                 "root_dir"
             ],
-            "word_emb_size": self.config["word_emb_size"],
-            "topology_subdir": self.config["graph_construction_args"]["graph_construction_share"][
+            "topology_subdir": self.config["model_args"]["graph_construction_args"]["graph_construction_share"][
                 "topology_subdir"
             ],
-            "edge_strategy": self.config["graph_construction_args"]["graph_construction_private"][
+            "word_emb_size": self.config["preprocessing_args"]["word_emb_size"],
+            "merge_strategy": self.config["model_args"]["graph_construction_args"]["graph_construction_private"][
+                "merge_strategy"
+            ],
+            "edge_strategy": self.config["model_args"]["graph_construction_args"]["graph_construction_private"][
                 "edge_strategy"
             ],
-            "graph_name": self.config["graph_construction_args"]["graph_construction_share"][
-                "graph_name"
+            "graph_construction_name": self.config["model_args"][
+                "graph_construction_name"
             ],
-            "share_vocab": self.config["share_vocab"],
-            "min_word_vocab_freq": self.config["min_word_freq"],
-            "thread_number": self.config["graph_construction_args"]["graph_construction_share"][
+            "share_vocab": self.config["preprocessing_args"]["share_vocab"],
+            "min_word_vocab_freq": self.config["preprocessing_args"]["min_word_freq"],
+            "thread_number": self.config["model_args"]["graph_construction_args"]["graph_construction_share"][
                 "thread_number"
             ],
-            "port": self.config["graph_construction_args"]["graph_construction_share"]["port"],
-            "timeout": self.config["graph_construction_args"]["graph_construction_share"][
+            "port": self.config["model_args"]["graph_construction_args"]["graph_construction_share"]["port"],
+            "timeout": self.config["model_args"]["graph_construction_args"]["graph_construction_share"][
                 "timeout"
             ],
             "tokenizer": None,
-            "for_inference": 1,
+            "for_inference": True,
             "reused_vocab_model": self.model.vocab,
         }
 
@@ -94,36 +83,21 @@ class ModelHandler:
 
         self.inference_data_loader = DataLoader(
             inference_dataset.test,
-            batch_size=self.config["batch_size"],
+            batch_size=self.config["training_args"]["batch_size"],
             shuffle=False,
-            num_workers=0,
+            num_workers=self.config["env_args"]["num_workers"],
             collate_fn=inference_dataset.collate_fn,
         )
 
         self.vocab = self.model.vocab
-        self.src_vocab = self.vocab.in_word_vocab
-        self.tgt_vocab = self.vocab.out_word_vocab
-
         self.num_test = len(inference_dataset.test)
+        print("Test size: {}".format(self.num_test))
+        self.logger.write("Test size: {}".format(self.num_test))
 
     def _build_model(self):
-        self.model = SumModel.load_checkpoint(self.stopper.save_model_path)
-        # self.logger.write(str(self.model))
-
-    def _build_optimizer(self):
-        parameters = [p for p in self.model.parameters() if p.requires_grad]
-        self.optimizer = optim.Adam(parameters, lr=self.config["lr"])
-        self.stopper = EarlyStopping(
-            os.path.join(self.config["out_dir"], Constants._SAVED_WEIGHTS_FILE),
-            patience=self.config["patience"],
-        )
-        self.scheduler = ReduceLROnPlateau(
-            self.optimizer,
-            mode="max",
-            factor=self.config["lr_reduce_factor"],
-            patience=self.config["lr_patience"],
-            verbose=True,
-        )
+        self.model = torch.load(
+            os.path.join(self.config["checkpoint_args"]["out_dir"], Constants._SAVED_WEIGHTS_FILE)
+        ).to(self.config["env_args"]["device"])
 
     def _build_evaluation(self):
         self.metrics = {"ROUGE": ROUGE()}
@@ -135,29 +109,32 @@ class ModelHandler:
             gt_collect = []
             for i, data in enumerate(dataloader):
                 print(i)
-                data = all_to_cuda(data, self.config["device"])
-                data["graph_data"] = data["graph_data"].to(self.config["device"])
+                data = all_to_cuda(data, self.config["env_args"]["device"])
+                data["graph_data"] = data["graph_data"].to(self.config["env_args"]["device"])
 
                 if self.use_copy:
                     oov_dict = prepare_ext_vocab(
-                        data["graph_data"], self.vocab, device=self.config["device"]
+                        data["graph_data"], self.vocab, device=self.config["env_args"]["device"]
                     )
                     ref_dict = oov_dict
                 else:
                     oov_dict = None
                     ref_dict = self.vocab.out_word_vocab
+
                 batch_graph = self.model.g2s.graph_initializer(data["graph_data"])
                 prob = self.model.g2s.encoder_decoder_beam_search(
-                    batch_graph, self.config["beam_size"], topk=1, oov_dict=oov_dict
+                    batch_graph, self.config["inference_args"]["beam_size"], topk=1, oov_dict=oov_dict
                 )
 
                 pred_ids = (
                     torch.zeros(
                         len(prob),
-                        self.config["decoder_args"]["rnn_decoder_private"]["max_decoder_step"],
+                        self.config["model_args"]["decoder_args"]["rnn_decoder_private"][
+                            "max_decoder_step"
+                        ],
                     )
                     .fill_(ref_dict.EOS)
-                    .to(self.config["device"])
+                    .to(self.config["env_args"]["device"])
                     .int()
                 )
                 for i, item in enumerate(prob):
@@ -174,9 +151,9 @@ class ModelHandler:
             if write2file:
                 with open(
                     "{}/{}_bs{}_pred.txt".format(
-                        self.config["out_dir"],
-                        self.config["out_dir"].split("/")[-1],
-                        self.config["beam_size"],
+                        self.config["checkpoint_args"]["out_dir"],
+                        self.config["checkpoint_args"]["out_dir"].split("/")[-1],
+                        self.config["inference_args"]["beam_size"],
                     ),
                     "w+",
                 ) as f:
@@ -185,9 +162,9 @@ class ModelHandler:
 
                 with open(
                     "{}/{}_bs{}_gt.txt".format(
-                        self.config["out_dir"],
-                        self.config["out_dir"].split("/")[-1],
-                        self.config["beam_size"],
+                        self.config["checkpoint_args"]["out_dir"],
+                        self.config["checkpoint_args"]["out_dir"].split("/")[-1],
+                        self.config["inference_args"]["beam_size"],
                     ),
                     "w+",
                 ) as f:
@@ -229,43 +206,22 @@ class ModelHandler:
         return format_str[:-1]
 
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-task_config", "--task_config", required=True, type=str, help="path to the config file"
-    )
-    parser.add_argument(
-        "-g2s_config", "--g2s_config", required=True, type=str, help="path to the config file"
-    )
-    parser.add_argument("--grid_search", action="store_true", help="flag: grid search")
-    args = vars(parser.parse_args())
-
-    return args
-
-
-def print_config(config):
-    print("**************** MODEL CONFIGURATION ****************")
-    for key in sorted(config.keys()):
-        val = config[key]
-        keystr = "{}".format(key) + (" " * (24 - len(key)))
-        print("{} -->   {}".format(keystr, val))
-    print("**************** MODEL CONFIGURATION ****************")
-
-
 def main(config):
     # configure
-    np.random.seed(config["seed"])
-    torch.manual_seed(config["seed"])
+    np.random.seed(config["env_args"]["seed"])
+    torch.manual_seed(config["env_args"]["seed"])
 
-    if not config["no_cuda"] and torch.cuda.is_available():
+    if not config["env_args"]["no_cuda"] and torch.cuda.is_available():
         print("[ Using CUDA ]")
-        config["device"] = torch.device("cuda" if config["gpu"] < 0 else "cuda:%d" % config["gpu"])
+        config["env_args"]["device"] = torch.device(
+            "cuda" if config["env_args"]["gpu"] < 0 else "cuda:%d" % config["env_args"]["gpu"]
+        )
         cudnn.benchmark = True
-        torch.cuda.manual_seed(config["seed"])
+        torch.cuda.manual_seed(config["env_args"]["seed"])
     else:
-        config["device"] = torch.device("cpu")
+        config["env_args"]["device"] = torch.device("cpu")
 
-    print("\n" + config["out_dir"])
+    print("\n" + config["checkpoint_args"]["out_dir"])
 
     runner = ModelHandler(config)
     t0 = time.time()
@@ -280,16 +236,33 @@ def main(config):
     return test_scores
 
 
+################################################################################
+# ArgParse and Helper Functions #
+################################################################################
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-json_config",
+        "--json_config",
+        required=True,
+        type=str,
+        help="path to the json config file",
+    )
+    args = vars(parser.parse_args())
+    return args
+
+
+def print_config(config):
+    print("**************** MODEL CONFIGURATION ****************")
+    for key in sorted(config.keys()):
+        val = config[key]
+        keystr = "{}".format(key) + (" " * (24 - len(key)))
+        print("{} -->  {}".format(keystr, val))
+    print("**************** MODEL CONFIGURATION ****************")
+
+
 if __name__ == "__main__":
     cfg = get_args()
-    task_args = get_yaml_config(cfg["task_config"])
-    g2s_args = get_yaml_config(cfg["g2s_config"])
-    # load Graph2Seq template config
-    g2s_template = get_basic_args(
-        graph_construction_name=g2s_args["graph_construction_name"],
-        graph_embedding_name=g2s_args["graph_embedding_name"],
-        decoder_name=g2s_args["decoder_name"],
-    )
-    update_values(to_args=g2s_template, from_args_list=[g2s_args, task_args])
-    print_config(g2s_template)
-    main(g2s_template)
+    config = load_json_config(cfg["json_config"])
+
+    main(config)
