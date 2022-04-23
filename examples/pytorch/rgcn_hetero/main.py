dbb12429986f6d7d1e1b35f86dc1c645d18fa529
Shaw Liu <shawxiaoliu@gmail.com>
2022-04-23 18:30:07 -0400

    RGCN wrap up

--- examples/pytorch/rgcn_hetero/main.py
+++ examples/pytorch/rgcn_hetero/main.py
@@ -1,63 +1,36 @@
+"""Deprecated RGCN code on Heterogeneous graph due to 
+the lack of support from data structure. The following supports
+are needed (but not limit to):
+- Redefine the feature data structure of node/edge
+    - Index node/edge ids by their type.
+    - Enable type indexed features.
+- Make corresponding changes on views.
+- Make corresponding changes on set/get features functions.
+
+This example bypasses it by storing the features in the model
+itself. It is a code trick and therefore not recommended to
+the user.
+"""
 import argparse
 import torch
 import dgl
-import time
 import torch.nn.functional as F
 from torchmetrics.functional import accuracy
-from rgcn import RGCN
+from rgcn_hetero import RGCNHetero
 from dgl.data.rdf import AIFBDataset, MUTAGDataset, BGSDataset, AMDataset
 from graph4nlp.pytorch.data.data import from_dgl
 
-# Fix random seed
-# torch.manual_seed(1024)
-# import random
-# random.seed(1024)
-# import numpy as np
-# np.random.seed(1024)
-
 # Load dataset 
 # Reference: dgl/examples/pytorch/rgcn/entity_utils.py (https://github.com/dmlc/dgl/blob/master/examples/pytorch/rgcn/entity_utils.py)
-def load_data(data_name='aifb', get_norm=False, inv_target=False):
+def load_data(data_name='aifb', get_norm=False):
     if data_name == 'aifb':
         dataset = AIFBDataset()
-        # Test Accuracy:
-        # 0.9444, 0.8889, 0.9722, 0.9167, 0.9444 without enorm.
-        # 0.8611, 0.8889, 0.8889, 0.8889, 0.8333
-        # avg: 0.93332 (without enorm)
-        # avg: 0.87222
-        # DGL: 0.8889, 0.8889, 0.8056, 0.8889, 0.8611
-        # DGL avg: 0.86668
-        # paper: 0.9583
-        # note: Could stuck at Local minimum of train loss between 0.2-0.35.
     elif data_name == 'mutag':
         dataset = MUTAGDataset()
-        # Test Accuracy:
-        # 0.6912, 0.7500, 0.7353, 0.6324, 0.7353
-        # avg: 0.68884
-        # DGL: 0.6765, 0.7059, 0.7353, 0.6765, 0.6912
-        # DGL avg: 0.69724
-        # paper: 0.7323
-        # note: Could stuck at local minimum of train acc: 0.3897 & loss 0.6931
     elif data_name == 'bgs':
         dataset = BGSDataset()
-        # Test Accuracy:
-        # 0.8966, 0.9310, 0.8966, 0.7931, 0.8621
-        # avg: 0.87588
-        # DGL: 0.7931, 0.9310, 0.8966, 0.8276, 0.8966
-        # DGL avg: 0.86898          
-        # paper: 0.8310
-        # note: Could stuck at local minimum of train acc: 0.6325 & loss: 0.6931
     else:
         dataset = AMDataset()
-        # Test Accuracy:
-        # 0.7525, 0.7374, 0.7424, 0.7424, 0.7424
-        # avg: 0.74342
-        # DGL: 0.7677, 0.7677, 0.7323, 0.7879, 0.7677
-        # DGL avg: 0.76466
-        # paper: 0.8929
-        # note: args.hidden_size is 10. 
-        # Could stuck at local minimum of train loss: 0.3-0.5
-
     # Load hetero-graph
     hg = dataset[0]
 
@@ -79,34 +52,30 @@ def load_data(data_name='aifb', get_norm=False, inv_target=False):
     else:
         edata = None
     category_id = hg.ntypes.index(category)
-    g = dgl.to_homogeneous(hg, edata=edata)
+    hg.ndata.pop('label')
+    # g = dgl.to_homogeneous(hg, edata=edata)
+    g = hg
     node_ids = torch.arange(g.num_nodes())
 
     # find out the target node ids in g
     loc = (g.ndata['_TYPE'] == category_id)
     target_idx = node_ids[loc]
 
-    if inv_target:
-        # Map global node IDs to type-specific node IDs. This is required for
-        # looking up type-specific labels in a minibatch
-        inv_target = torch.empty((g.num_nodes(),), dtype=torch.int64)
-        inv_target[target_idx] = torch.arange(0, target_idx.shape[0],
-                                           dtype=inv_target.dtype)
-        return g, num_rels, num_classes, labels, train_idx, test_idx, target_idx, inv_target
-    else:
-        return g, num_rels, num_classes, labels, train_idx, test_idx, target_idx
+    return g, category, num_rels, num_classes, labels, train_idx, test_idx, target_idx
 
 
 def main(args):
-    g, num_rels, num_classes, labels, train_idx, test_idx, target_idx = load_data(data_name=args.dataset, get_norm=True)
+    g, category, num_rels, num_classes, labels, train_idx, test_idx, target_idx = load_data(data_name=args.dataset, get_norm=True)
 
-    graph = from_dgl(g, is_hetero=False)
+    graph = from_dgl(g, is_hetero=True)
     num_nodes = graph.get_node_num()
-    model = RGCN(num_hidden_layers=args.num_hidden_layers, 
+    model = RGCNHetero(num_hidden_layers=args.num_hidden_layers, 
                  input_size=num_nodes,
                  hidden_size=args.hidden_size,
                  output_size=num_classes,
-                 num_rels=num_rels,
+                 rel_names=list(set(g.etypes)),
+                 node_types=list(graph.ntypes),
+                 num_nodes={nt: len(g.ndata['_ID'][nt]) for nt in graph.ntypes},
                  num_bases=args.num_bases,
                  use_self_loop=args.use_self_loop,
                  gpu=args.gpu,
@@ -115,8 +84,7 @@ def main(args):
     print("start training...")
     model.train()
     for epoch in range(args.num_epochs):
-        logits = model(graph).node_features["node_emb"]
-        logits = logits[target_idx]
+        logits = model(graph)[category]
         loss = F.cross_entropy(logits[train_idx], labels[train_idx])
         optimizer.zero_grad()
         loss.backward()
@@ -130,8 +98,7 @@ def main(args):
     print("start evaluating...")
     model.eval()
     with torch.no_grad():
-        logits = model(graph).node_features["node_emb"]
-    logits = logits[target_idx]
+        logits = model(graph)[category]
     test_acc = accuracy(logits[test_idx].argmax(dim=1), labels[test_idx]).item()
     print("Test Accuracy: {:.4f}".format(test_acc))
 
@@ -146,7 +113,7 @@ if __name__ == "__main__":
                         help="GPU device number, -1 for cpu")
     parser.add_argument("--num-bases", type=int, default=-1,
                         help="number of filter weight matrices, default: -1 [use all]")
-    parser.add_argument("-d", "--dataset", type=str, required=True,
+    parser.add_argument("-d", "--dataset", type=str, default='aifb',
                         choices=['aifb', 'mutag', 'bgs', 'am'],
                         help="dataset to use")
     parser.add_argument("--use-self-loop", type=bool, default=False,
@@ -163,21 +130,3 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print(args)
     main(args)
-
-
-
-
-
-"""Deprecated RGCN code on Heterogeneous graph due to 
-the lack of support from data structure. The following supports
-are needed (but not limit to):
-- Redefine the feature data structure of node/edge
-    - Index node/edge ids by their type.
-    - Enable type indexed features.
-- Make corresponding changes on views.
-- Make corresponding changes on set/get features functions.
-
-This example bypasses it by storing the features in the model
-itself. It is a code trick and therefore not recommended to
-the user.
-"""
